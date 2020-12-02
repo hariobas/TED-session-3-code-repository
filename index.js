31e545bf6708aa3fb15916acdb6a1c33ac739e7b
hariobas <basoeki.hario@gmail.com>
2020-12-02 18:24:08 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const port = 3000
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.listen(port, () => console.log(`Hello world app, listening on port ${port}!`))
